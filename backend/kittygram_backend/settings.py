04d9cc0a581359f519daa307b8b90ef09742baaa
bakhvalov4 <bakhvalov6@yandex.ru>
2025-03-14 11:43:38 +0000

    infra_sprint1-3

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
